40b87abb7b9c57fc2d6b9e95fef79c3a6fe99311
andres-geotec <andres.geotec@gmail.com>
2022-09-02 00:18:09 -0500

    configuracipon basica para despliegue

---
+++ vuepress.config.js
@@ -0,0 +1,9 @@
+import { defineUserConfig } from "vuepress";
+
+export default defineUserConfig({
+  base: "/notas-dev",
+  lang: "es-MX",
+  title: "Notas dev",
+  description:
+    "Este proyecto está pensado para tener acceso simple a todos los comandos aprendidos durante la profesión como desarrollador.",
+});
